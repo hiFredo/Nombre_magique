5621db0b9ce2322f0f3da2b089a6092cf3efd23d
Mazino <fredozv01@gmail.com>
2025-01-01 01:50:32 +0100

    defait

--- main.py
+++ main.py
@@ -19,24 +19,46 @@ def demander_nombre(nb_min,nb_max):
                 print(f"ERREUR : Le  nombre doit etre  entre ({nb_min} et {nb_max})  ") 
                 nombre_int = 0 
     return nombre_int   
-nombre = 0 
-while not nombre==NOMBRE_MAGIQUE and NB_VIE>0  :
+# nombre = 0 
+# while not nombre==NOMBRE_MAGIQUE and NB_VIE>0  :
+#     nombre = demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
+#     print(f"Il vous reste {NB_VIE} vie ")
+#     if nombre==NOMBRE_MAGIQUE :
+#         print("Bravo, vous avez gagné !")
+
+#     elif nombre<NOMBRE_MAGIQUE : 
+#         print("Le nombre magique est plus grand")
+#         NB_VIE-=1
+
+#     else :
+#         print("Le nombre magique est plus petiT")
+#         NB_VIE-=1
+    
+# if NB_VIE == 0 :
+#     print(f"Vous avez perdue ! le nombre magique était : {NOMBRE_MAGIQUE} ") 
+
+gagne=False
+for i in range(0,NB_VIE):
+    vies= NB_VIE-i
     nombre = demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
-    print(f"Il vous reste {NB_VIE} vie ")
+    print(f"Il vous reste {vies} vie ")
     if nombre==NOMBRE_MAGIQUE :
         print("Bravo, vous avez gagné !")
+        gagne=True
+        break
 
     elif nombre<NOMBRE_MAGIQUE : 
         print("Le nombre magique est plus grand")
-        NB_VIE-=1
+        
 
     else :
         print("Le nombre magique est plus petiT")
-        NB_VIE-=1
-    
-if NB_VIE == 0 :
-    print(f"Vous avez perdue ! le nombre magique était : {NOMBRE_MAGIQUE} ")    
         
+    
+# if vies == 0 :
+if not gagne : 
+    print(f"Vous avez perdue ! le nombre magique était : {NOMBRE_MAGIQUE} ")   
+            
 
 
 
